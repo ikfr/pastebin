08062c512641a4ba99179be820627df52556eab0
hzqd <hzqelf@yeah.net>
2020-04-17 02:57:11 +0800

    optimized

--- src/main/kotlin/com/github/hzqd/ikfr/pastebin/App.kt
+++ src/main/kotlin/com/github/hzqd/ikfr/pastebin/App.kt
@@ -1,34 +1,34 @@
 package com.github.hzqd.ikfr.pastebin
 
-import com.google.common.hash.Hashing
+import arrow.core.None
+import arrow.core.Option
+import arrow.core.Some
 import io.vertx.core.Vertx
-import io.vertx.core.http.HttpHeaders
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
 import org.redisson.Redisson
+import org.redisson.api.RMap
 import org.redisson.config.Config
-import java.lang.Exception
-import java.nio.charset.StandardCharsets
-import java.util.*
-import kotlin.math.absoluteValue
-import kotlin.random.Random
-import kotlin.random.nextUInt
 
 object App {
-    val redissonClient = Config().let {
-        it.useSingleServer()
-            .setAddress("redis://127.0.0.1:6379")
-        Redisson.create(it)
+    val redissonMap: Option<RMap<String, String>> = Option(
+        Config().let {
+            it.useSingleServer().address = "redis://127.0.0.1:6379"
+            Redisson.create(it)
+        }.getMap("content")
+    )
+
+    fun set(k: String, v: String) {
+        when (redissonMap) {
+            is Some<RMap<String, String>> -> redissonMap.t[k] = v
+            is None -> println("None")
+        }
     }
 
-
-    val redissonMap = redissonClient.getMap<String,String>("content")
-    fun set(k:String, v:String) {
-        redissonMap[k] = v
-    }
-    fun get(k:String) : String?{
-        return redissonMap[k]
+    fun get(k: String): String = when (redissonMap) {
+        is Some<RMap<String, String>> -> redissonMap.t[k] ?: "None"
+        is None -> "None"
     }
 
 
@@ -45,27 +45,31 @@ object App {
     }
 
     fun index(context: RoutingContext) {
-        context.response().end("<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    <title>Document</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "    <form action=\"paste\" method=\"post\">\n" +
-                "        <input type=\"text\" name=\"content\">\n" +
-                "        <input type=\"submit\">\n" +
-                "    </form>\n" +
-                "</body>\n" +
-                "</html>")
+        context.response().end(
+            "<!DOCTYPE html>\n" +
+                    "<html lang=\"en\">\n" +
+                    "<head>\n" +
+                    "    <meta charset=\"UTF-8\">\n" +
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                    "    <title>Document</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "    <form action=\"paste\" method=\"post\">\n" +
+                    "        <input type=\"text\" name=\"content\">\n" +
+                    "        <input type=\"submit\">\n" +
+                    "    </form>\n" +
+                    "</body>\n" +
+                    "</html>"
+        )
     }
+
     fun paste(context: RoutingContext) = with(context) {
         val content = request().getParam("content")
-        val key =  Integer.toHexString(content.hashCode()).slice((0..5))
+        val key = Integer.toHexString(content.hashCode()).slice((0..5))
         set(key, content)
         response().putHeader("location", key).setStatusCode(302).end()
     }
+
     fun query(context: RoutingContext) {
         try {
             val content = get(context.request().uri().toString().slice(1..6))
